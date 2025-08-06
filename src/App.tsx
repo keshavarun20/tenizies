d0d9b25df5ed3661cff47dddafcae6e3c677997a
B1ACKCAP365 <keshavarun1220@gmail.com>
2025-08-06 13:28:33 +0530

    Refactor comments in App component for clarity on array creation

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Die from "./components/Die";
 const App = () => {
-  const random = () => Math.floor(Math.random() * 6) + 1;
-  const allNewDice = () => Array.from({ length: 10 }, random);
+const random = () => Math.floor(Math.random() * 6) +1;  
+//creates a new array from an array-like or iterable object.
+
+// Array-like object: an object that has a length property and indexed elements (like arguments or NodeList).
+
+// Iterable: something you can loop over with for...of (like strings, arrays, sets, etc.).
+
+const allNewDice = () => Array.from({ length: 10 }, random);
   
   const [dice, setDice] = useState(allNewDice())
   const box = dice.map((dice, index) => <Die key={index} value={dice} onClick={() => setDice(allNewDice())}
